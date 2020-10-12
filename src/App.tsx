2a62ea26e72abef283883207917927a012b2c1e9
Michel Victor <michelvictor.doc@gmail.com>
2020-10-12 13:05:40 -0300

    init project

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import './styles/global.css'
+import './styles/pages/landing.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id='page-landing'>
+      <h1>Hello NLW</h1>
     </div>
   );
 }
