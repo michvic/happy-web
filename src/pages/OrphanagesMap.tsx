c2d95701a2b4024ab12d6f42d0214b23a02b9803
Michel Victor <michelvictor.doc@gmail.com>
2020-10-16 10:25:27 -0300

    add map

--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { FiPlus } from 'react-icons/fi'
+import { Map, TileLayer } from 'react-leaflet'
+
+import 'leaflet/dist/leaflet.css'
 
 import mapMakerImg from '../image/map-marker.svg'
 
@@ -22,7 +25,14 @@ function OrphanagesMap() {
         </footer>
       </aside>
 
-      <div></div>
+      <Map 
+        center={[-5.3591909,-49.0312173]}
+        zoom={15}
+        style={{ width:'100%', height:'100%'}}
+      >
+        {/* <TileLayer url='https://a.tile.openstreetmap.org/{z}/{x}/{y}.png' /> */}
+        <TileLayer url={`https://api.mapbox.com/styles/v1/mapbox/light-v10/tiles/256/{z}/{x}/{y}@2x?access_token=${process.env.REACT_APP_MAPBOX_TOKEN}`}/>
+      </Map>
 
       <Link to='' className='create-orphanage'>
         <FiPlus size={32} color='#FFF'/>
